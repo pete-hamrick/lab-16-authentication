92cf0277f33ae03d0ed4b3baed984dcc97ba1440
Pete Hamrick <pete.hamrick@gmail.com>
2021-10-07 13:33:55 -0700

    made auth controller

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,7 +10,7 @@ describe('authentication routes', () => {
 
   it('signs up a user and returns that users id using POST /signup', async () => {
     const res = await request(app)
-      .post('/signup')
+      .post('api/auth/signup')
       .send({ email: 'me@you.com', password: 'usandthem' });
     expect(res.body).toEqual({ userId: '1' });
   });
