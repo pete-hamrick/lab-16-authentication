96bf907e8b7185bd0cb898cf9ccfc9af3ee1b115
Pete Hamrick <pete.hamrick@gmail.com>
2021-10-11 14:29:37 -0700

    added standardUser to test file, broke some tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,23 +4,23 @@ const request = require('supertest');
 const app = require('../lib/app.js');
 const UserService = require('../lib/services/UserService.js');
 
+const standardUser = {
+  email: 'me@you.com',
+  password: 'usandthem',
+};
+
 describe('authentication routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
   it('signs up a user and returns that users id using POST /signup', async () => {
-    const res = await request(app)
-      .post('/api/auth/signup')
-      .send({ email: 'me@you.com', password: 'usandthem' });
+    const res = await request(app).post('/api/auth/signup').send(standardUser);
     expect(res.body).toEqual({ id: '1' });
   });
 
   it('returns 400 for trying to create new user where email already exists', async () => {
-    await UserService.create({
-      email: 'me@you.com',
-      password: 'usandthem',
-    });
+    await UserService.create(standardUser);
 
     const res = await request(app)
       .post('/api/auth/signup')
@@ -30,23 +30,15 @@ describe('authentication routes', () => {
   });
 
   it('logs a user in returning the users id', async () => {
-    await UserService.create({
-      email: 'me@you.com',
-      password: 'usandthem',
-    });
+    await UserService.create(standardUser);
 
-    const res = await request(app)
-      .post('/api/auth/login')
-      .send({ email: 'me@you.com', password: 'usandthem' });
+    const res = await request(app).post('/api/auth/login').send(standardUser);
 
     expect(res.body).toEqual({ id: '1' });
   });
 
   it('should return 401 if bad email or password provided', async () => {
-    await UserService.create({
-      email: 'me@you.com',
-      password: 'usandthem',
-    });
+    await UserService.create(standardUser);
 
     const res = await request(app)
       .post('/api/auth/login')
@@ -56,10 +48,7 @@ describe('authentication routes', () => {
   });
 
   it('returns the currently logged in user on the GET /me route', async () => {
-    await UserService.create({
-      email: 'me@you.com',
-      password: 'usandthem',
-    });
+    await UserService.create(standardUser);
 
     const agent = request.agent(app);
 
